a1e17f8a4dce399dc45ac8c9a6daf44d3dba5229
Ravy Freitas <ravy.freitas@meta3.com.br>
2019-07-03 11:20:23 -0300

    configurações eslint

---
+++ src/components/Map/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-native/split-platform-components */
+import React from 'react';
+import MapView from 'react-native-maps';
+import { PermissionsAndroid, View } from 'react-native';
+// import { Container } from './styles';
+
+const Map = () => (
+  <View style={{ flex: 1 }}>
+    <MapView
+      onMapReady={() => {
+        PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+      }}
+      style={{ flex: 1 }}
+      region={{
+        latitude: -19.9640335,
+        longitude: -43.993464,
+        latitudeDelta: 0.0143,
+        longitudeDelta: 0.0134,
+      }}
+      showsUserLocation
+      showsMyLocationButton
+    />
+  </View>
+);
+export default Map;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,11 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import MapView from 'react-native-maps';
-import { PermissionsAndroid, View } from 'react-native';
+import { View } from 'react-native';
+import Map from '../../components/Map';
 
 
 const Main = () => (
   <View style={{ flex: 1 }}>
-    <MapView
-      onMapReady={() => {
-        PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        );
-      }}
-      style={{ flex: 1 }}
-      region={{
-        latitude: -19.9640335,
-        longitude: -43.993464,
-        latitudeDelta: 0.0143,
-        longitudeDelta: 0.0134,
-      }}
-      showsUserLocation
-      showsMyLocationButton
-    />
+    <Map />
   </View>
 );
 
