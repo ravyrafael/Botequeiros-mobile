4d0fe6136ebd42446120c0439374a552bad21e22
ravyrafael <ravyrafael@gmail.com>
2019-07-04 14:03:48 -0300

    mock

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,8 +4,8 @@
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { PermissionsAndroid, View, Image } from 'react-native';
-import styles , {customMap }from './styles';
- import places from '../../mock/places';
+import styles, { customMap } from './styles';
+import places from '../../mock/places';
 // import Geocoder from 'react-native-geocoding';
 // import { Container } from './styles';
 const BarIcon = require('./beer.png')
@@ -19,50 +19,62 @@ export default class Map extends React.Component {
       latitudeDelta: 0.0143,
       longitudeDelta: 0.0134,
     },
-    deltaDefault:{
+    locationDefault: {
+      latitude: null,
+      longitude: null,
       latitudeDelta: 0.0143,
       longitudeDelta: 0.0134,
     }
     ,
-    places:[]
+    places: []
 
   };
   calculateDistance(lat1, lon1, lat2, lon2, unit) {
-    var radlat1 = Math.PI * lat1/180
-    var radlat2 = Math.PI * lat2/180
-    var radlon1 = Math.PI * lon1/180
-    var radlon2 = Math.PI * lon2/180
-    var theta = lon1-lon2
-    var radtheta = Math.PI * theta/180
+    var radlat1 = Math.PI * lat1 / 180
+    var radlat2 = Math.PI * lat2 / 180
+    var radlon1 = Math.PI * lon1 / 180
+    var radlon2 = Math.PI * lon2 / 180
+    var theta = lon1 - lon2
+    var radtheta = Math.PI * theta / 180
     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     dist = Math.acos(dist)
-    dist = dist * 180/Math.PI
+    dist = dist * 180 / Math.PI
     dist = dist * 60 * 1.1515
-    if (unit=="K") { dist = dist * 1.609344 }
-    if (unit=="N") { dist = dist * 0.8684 }
+    if (unit == "K") { dist = dist * 1.609344 }
+    if (unit == "N") { dist = dist * 0.8684 }
     return dist
   }
 
-  async componentDidMount() {
+  async updateLocation(first = false) {
     await navigator.geolocation.getCurrentPosition(
       ({ coords: { latitude, longitude } }) => {
         // const response = await Geocoder.from({ latitude, longitude });
         // const address = response.results[0].formatted_address;
         // const location = address.substring(0, address.indexOf(','));
-
+        if (!first) {
+          this.setState({
+            region: {
+              latitude,
+              longitude,
+              latitudeDelta: 0.0143,
+              longitudeDelta: 0.0134,
+            },
+          });
+        }
         this.setState({
-          region: {
+          locationDefault: {
             latitude,
             longitude,
             latitudeDelta: 0.0143,
             longitudeDelta: 0.0134,
-          },
+          }
         });
         this.setState({
-          places: places.map(place=>({
-            ...place, 
-            distance:this.calculateDistance(latitude, longitude,place.location.latitude, place.location.longitude, 'k')}))
-            .sort((a,b)=>(a.distance-b.distance))
+          places: places.map(place => ({
+            ...place,
+            distance: this.calculateDistance(latitude, longitude, place.location.latitude, place.location.longitude, 'k')
+          }))
+            .sort((a, b) => (a.distance - b.distance))
         });
       }, // sucesso
       () => { }, // erro
@@ -72,7 +84,9 @@ export default class Map extends React.Component {
         maximumAge: 1000,
       },
     );
-
+  }
+  async componentDidMount() {
+      await this.updateLocation()
   }
 
   render() {
@@ -80,8 +94,7 @@ export default class Map extends React.Component {
     return (
       <View style={styles.map}>
         <MapView
-        customMapStyle={customMap}
-        onRegionChangeComplete={x=>console.log(x)}
+          customMapStyle={customMap}
           onMapReady={() => {
             PermissionsAndroid.request(
               PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -90,7 +103,7 @@ export default class Map extends React.Component {
             });
           }}
           style={{ ...styles.map, marginBottom }}
-          region={region}
+          region={this.state.region}
           showsMyLocationButton
           showsUserLocation
           loadingEnabled
@@ -101,13 +114,13 @@ export default class Map extends React.Component {
               key={place.key}
               coordinate={place.location}
               title={place.name}
-              description={`distancia: ${place.distance.toFixed(3)}`}       
+              description={`distancia: ${place.distance.toFixed(3)}`}
             >
               <Image
                 source={BarIcon}
-                style={{height: 40, width: 40}}
-            />
-              </Marker>
+                style={{ height: 40, width: 40 }}
+              />
+            </Marker>
           ))}
         </MapView>
       </View>
--- src/mock/places/index.js
+++ src/mock/places/index.js
@@ -108,5 +108,467 @@
   },
   beer: 5.50,
 },
+{
+  key:11,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8805239,
+    longitude: -43.950824
+  },
+  beer: 5.50,
+},
+{
+  key:12,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8805236,
+    longitude: -43.9508266,
+  },
+  beer: 5.50,
+},
+{
+  key:13,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8927261,
+    longitude: -43.9491317
+  },
+  beer: 5.50,
+},
+{
+  key:14,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8927251,
+    longitude: -43.9491327
+  },
+  beer: 5.50,
+},
+{
+  key:15,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9040869,
+    longitude: -43.951233
+  },
+  beer: 5.50,
+},
+{
+  key:16,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8966778,
+    longitude: -43.9475558
+  },
+  beer: 5.50,
+},
+{
+  key:17,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.890549,
+    longitude: -43.9426376
+  },
+  beer: 5.50,
+},
+{
+  key:18,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8848676,
+    longitude: -43.9623713
+  },
+  beer: 5.50,
+},
+{
+  key:19,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.888216,
+    longitude: -43.9290749
+  },
+  beer: 5.50,
+},
+{
+  key:20,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8660092,
+    longitude: -43.9145819
+  },
+  beer: 5.50,
+},
+{
+  key:21,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9240741,
+    longitude: -43.9441922
+  },
+  beer: 5.50,
+},
+{
+  key:22,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8564393,
+    longitude: -43.9580215
+  },
+  beer: 5.50,
+},
+{
+  key:23,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8698351,
+    longitude: -43.9465895
+  },
+  beer: 5.50,
+},
+{
+  key:24,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8789402,
+    longitude: -43.9206192
+  },
+  beer: 5.50,
+},
+{
+  key:25,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8936851,
+    longitude: -43.9026903,
+  },
+  beer: 5.50,
+},
+{
+  key:26,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9104844,
+    longitude: -43.8999416
+  },
+  beer: 5.50,
+},
+{
+  key:27,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9349166,
+    longitude: -43.9171208
+  },
+  beer: 5.50,
+},
+{
+  key:28,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9486581,
+    longitude: -43.9419214
+  },
+  beer: 5.50,
+},
+{
+  key:29,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9653341,
+    longitude: -43.9610372
+  },
+  beer: 5.50,
+},
+{
+  key:30,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8905239,
+    longitude: -43.9208246
+  },
+  beer: 5.50,
+},
+{
+  key:31,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8505239,
+    longitude: -43.9908246
+  },
+  beer: 5.50,
+},
+{
+  key:32,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8955239,
+    longitude: -43.9278246
+  },
+  beer: 5.50,
+},
+{
+  key:33,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8105239,
+    longitude: -43.9108246
+  },
+  beer: 5.50,
+},
+{
+  key:34,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9654697,
+    longitude: -44.0304024
+  },
+  beer: 5.50,
+},
+{
+  key:35,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9054697,
+    longitude: -44.0904024
+  },
+  beer: 5.50,
+},
+{
+  key:36,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.3054697,
+    longitude: -44.0924924
+  },
+  beer: 5.50,
+},
+{
+  key:37,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8678174,
+    longitude: -44.0472058
+  },
+  beer: 5.50,
+},
+{
+  key:38,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8410962,
+    longitude: -44.0255765
+  },
+  beer: 5.50,
+},
+{
+  key:39,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8176956,
+    longitude: -43.9903055
+  },
+  beer: 5.50,
+},
+{
+  key:40,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.800657,
+    longitude: -44.0038667
+  },
+  beer: 5.50,
+},
+{
+  key:41,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8730587,
+    longitude: -44.0334859
+  },
+  beer: 5.50,
+},
+{
+  key:42,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8986234,
+    longitude: -44.0414075
+  },
+  beer: 5.50,
+},
+{
+  key:43,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8805239,
+    longitude: -43.9508246
+  },
+  beer: 5.50,
+},
+{
+  key:44,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8933238,
+    longitude: -44.0318697
+  },
+  beer: 5.50,
+},
+{
+  key:45,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8978534,
+    longitude: -44.0218983
+  },
+  beer: 5.50,
+},
+{
+  key:46,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9071839,
+    longitude: -44.0113007
+  },
+  beer: 5.50,
+},
+{
+  key:47,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9006163,
+    longitude: -44.001233
+  },
+  beer: 5.50,
+},
+{
+  key:48,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9537157,
+    longitude: -43.9827847
+  },
+  beer: 5.50,
+},
+{
+  key:49,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8805239,
+    longitude: -43.9508246
+  },
+  beer: 5.50,
+},
+{
+  key:50,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9389245,
+    longitude: -43.9413739
+  },
+  beer: 5.50,
+},
+{
+  key:51,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.8805239,
+    longitude: -43.9508246
+  },
+  beer: 5.50,
+},
+{
+  key:52,
+  name: 'Bar 1',
+  description: 'Bar com rodizio de pizzas, cerveja gelada e torres de chopp',
+  rating: [],
+  location: {
+    latitude: -19.9117289,
+    longitude: -43.9154024
+  },
+  beer: 5.50,
+},
 ];
 
